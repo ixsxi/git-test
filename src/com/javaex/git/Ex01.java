aa1fbeb6f24d6dfe1b9b008ea1d8862a947c634c
user <user@DESKTOP-PMC72TK>
2021-05-25 11:05:58 +0900

    5/1일 코딩

---
+++ src/com/javaex/git/Ex01.java
@@ -0,0 +1,11 @@
+package com.javaex.git;
+
+public class Ex01 {
+
+	public static void main(String[] args) {
+		System.out.println("5/1 코드");
+		System.out.println("5/2 코드");
+		System.out.println("5/3 코드");
+	}
+
+}
